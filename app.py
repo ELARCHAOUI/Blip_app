e852516dc15d46f72620643961569e3753095540
El_archaoui <mohamedelarchaoui2003@gmail.com>
2025-11-12 15:10:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -87,4 +87,6 @@ demo = gr.Interface(
 
 if __name__ == "__main__":
     # host="0.0.0.0" pour Render / HuggingFace / Docker
-    demo.launch(server_name="0.0.0.0", server_port=7860)
+    import os
+    demo.launch(server_name="0.0.0.0",server_port=int(os.environ.get("PORT", 7860)))
+
